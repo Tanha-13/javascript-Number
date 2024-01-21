f4bec86f0f8cfa65d94085cdde46ef6cd1dc69dc
Tanha <nusrattanha897@gmail.com>
2024-01-21 21:05:26 +0600

    methods:toFixed,toString,toPrecision

--- number-basics.js
+++ number-basics.js
@@ -9,3 +9,19 @@ const balance = new Number(4000);
 // console.log(Object.getPrototypeOf(balance));
 
 
+// topic: Number to String - toString
+// console.log(balance.toString()); // it can access the String built in methods
+// topic: toFixed
+// console.log(balance.toFixed(3));
+
+// topic: toPrecision
+// todo: toPrecision method in details
+const floatingNumber1 = 23.8966;
+const floatingNumber2 = 23.7866;
+const floatingNumber3 = 123.7866;
+const floatingNumber4 = 1123.7866;
+console.log(floatingNumber1.toPrecision(3));
+console.log(floatingNumber2.toPrecision(3));
+console.log(floatingNumber3.toPrecision(3));
+console.log(floatingNumber4.toPrecision(3));
+
