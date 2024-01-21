f48a79159a6655fc1c954d6d8d1b357603edf221
Tanha <nusrattanha897@gmail.com>
2024-01-21 20:42:49 +0600

    declaring number type variables and prototypes

---
+++ number-basics.js
@@ -0,0 +1,11 @@
+// topic: how to declare number type variable
+// implicitly
+const score = 400;
+// console.log(typeof score); //number
+// console.log(Object.getPrototypeOf(score));
+// explicity
+const balance = new Number(4000);
+// console.log(typeof balance); //object
+// console.log(Object.getPrototypeOf(balance));
+
+
